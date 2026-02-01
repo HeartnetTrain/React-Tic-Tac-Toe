21aa3ebbab4ce89bfa2f00874a602de10f645d91
Wael MESSAOUD / HeartnetTrain <messaoudwael2@gmail.com>
2026-02-01 17:43:24 +0100

    remove boilerplate

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,21 +3,17 @@ import styles from './Board.module.css';
 
 export default function Board() {
   return (
-  <div className={styles.board}>
-    <div className={styles.boardRow}>
-      <Square />
-      <Square />
-      <Square />
+    <div className={styles.board}>
+      {Array.from({ length: 3 }, (_, i) => (
+        <div key={`row-${i}`} className={styles.boardRow}>
+          {Array.from({ length: 3 }, (_, j) => {
+            const squareIndex = i * 3 + j; // Calculate 0 through 8
+            return (
+                <Square key={squareIndex}/>
+            );
+          })}
+        </div>
+      ))}
     </div>
-    <div className={styles.boardRow}>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className={styles.boardRow}>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-  </div>);
+  );
 }
