d0cfafba625f5f4e6b38a75ec8e8a732b7c1b03c
Wael MESSAOUD / HeartnetTrain <messaoudwael2@gmail.com>
2026-02-01 22:14:39 +0100

    determine winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,21 @@
 import Square from './Square';
 import styles from './Board.module.css';
 import { useState } from 'react';
+import calculateWinner from '../utils/calculateWinner';
 
 export default function Board() {
   const [squares,setSquares] = useState(Array.from(9).fill(null));
   const [isXNext,setIsXNext] = useState(true);
+  const [nextMove,setNextMove] = useState(0);
+
+  const winner = calculateWinner(squares);
+  let status = winner ? `Player ${winner} winned ! ` : nextMove < 9 ? `Next Player is: ${isXNext ? 'X': 'O'}` : "It's a draw !" ; 
 
   function handleClick(index) {
-    if(squares[index] ) {
+    if(squares[index] || winner) {
       return;
     }
+  
     const nextSquares = squares.slice();
 
     if(isXNext) {
@@ -20,9 +26,18 @@ export default function Board() {
     }
     setSquares(nextSquares);
     setIsXNext(!isXNext);
+    setNextMove(nextMove+1);
+  }
 
+  function resetBoard() {
+    setSquares(Array.from(9).fill(null));
+    setIsXNext(true);
+    setNextMove(0);
   }
   return (
+    <>
+    <div className="status">{status}</div>
+    <button onClick={resetBoard} disabled={!squares.length}> Reset Game </button>
     <div className={styles.board}>
       {Array.from({ length: 3 }, (_, i) => (
         <div key={`row-${i}`} className={styles.boardRow}>
@@ -37,5 +52,6 @@ export default function Board() {
         </div>
       ))}
     </div>
+    </>
   );
 }
