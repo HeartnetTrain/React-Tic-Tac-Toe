1cc1107d9969551808f14432a682ba0a55d8223c
Wael MESSAOUD / HeartnetTrain <messaoudwael2@gmail.com>
2026-02-01 20:05:08 +0100

    fill squares with X and O alternatively

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,27 @@
 import Square from './Square';
 import styles from './Board.module.css';
+import { useState } from 'react';
 
 export default function Board() {
+  const [squares,setSquares] = useState(Array.from(9).fill(null));
+  const [isXNext,setIsXNext] = useState(true);
+
+  function handleClick(index) {
+    if(squares[index] ) {
+      return;
+    }
+    const nextSquares = squares.slice();
+
+    if(isXNext) {
+        nextSquares[index] = 'X';
+    }
+    else {
+      nextSquares[index] = 'O';
+    }
+    setSquares(nextSquares);
+    setIsXNext(!isXNext);
+
+  }
   return (
     <div className={styles.board}>
       {Array.from({ length: 3 }, (_, i) => (
@@ -9,7 +29,9 @@ export default function Board() {
           {Array.from({ length: 3 }, (_, j) => {
             const squareIndex = i * 3 + j; // Calculate 0 through 8
             return (
-                <Square key={squareIndex}/>
+                <Square key={squareIndex} 
+                value = {squares[squareIndex]}
+                SquareBoxClicked = {() => handleClick(squareIndex)} />
             );
           })}
         </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,5 @@
 import styles from './Square.module.css';
 
-export default function Square() {
-  return <button className={styles.square}>X</button>;
+export default function Square({value,SquareBoxClicked}) {
+  return <button className={styles.square} onClick={SquareBoxClicked}>{value}</button>;
 }
